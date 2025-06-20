0b5eb87e63222fee99280cfd9d4b5c83ff107a8f
dikaenwo <rimaputri9879@gmail.com>
2025-06-20 21:31:56 +0800

    kantong darah

---
+++ app/src/main/java/com/example/sadarapp/ui/darah/CariKantongDarahActivity.kt
@@ -0,0 +1,21 @@
+package com.example.sadarapp.ui.darah
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sadarapp.R
+
+class CariKantongDarahActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_cari_kantong_darah)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
