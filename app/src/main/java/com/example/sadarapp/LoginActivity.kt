843be421470226910fe216fba7b409bed62a68a3
dikaenwo <rimaputri9879@gmail.com>
2025-06-20 19:17:39 +0800

    solve merge conflict

--- app/src/main/java/com/example/sadarapp/LoginActivity.kt
+++ app/src/main/java/com/example/sadarapp/LoginActivity.kt
@@ -1,6 +1,7 @@
 package com.example.sadar
 
 import android.os.Bundle
+import androidx.activity.R
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,6 @@ import androidx.core.view.WindowInsetsCompat
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
     }
 }
